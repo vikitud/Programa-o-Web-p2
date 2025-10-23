dbcfc9c6be87e4b8067a50e99e61e44ff1d43f0c
vikitud <victordanielb12@gmail.com>
2025-10-23 10:12:49 -0300

    n
    n

---
+++ tarefa.php/admin/form_clientes_alterar.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "config.inc.php";
+    $sql = "SELECT * FROM clientes WHERE id $_POST[id]";
+    $resultado = mysqli_querry($conexao, $sql);
+
+    while ($cliente = mysqli_fetch_array($resultado)){
+        $nome = $cliente['nome']
+    }
+
+<form action="?pg=altera_clientes method="post">
+    <label>Nome do Cliente:
+    <input type="text" name="nome" />
+    <label>cidade:</label>
+    <input type="text" name="cidade" />
+    <label>estado:</label>
+    <input type="text" name="estado" />
+    <input> 
+    </label>
+</form>
